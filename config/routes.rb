61999fa04eaa112bb18164729e623bb9e10f4a2a
kkeen699 <kkeen699@gmail.com>
2022-10-31 00:20:01 -0500

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  Rails.application.routes.draw do
-    resources :movies
-    root :to => redirect('/movies')
-  end
+  resources :movies
+  root :to => redirect('/movies')
 end
